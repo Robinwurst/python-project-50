a863d2744c34cf6372b94b533a44d05563cb548f
robert <Neorganizm911@yandex.ru>
2024-09-20 20:45:31 +0300

    test_gendiff

--- hexlet_code/test_gendiff.py
+++ hexlet_code/test_gendiff.py
@@ -1,5 +1,16 @@
 import pytest
 from hexlet_code import generate_diff
 
-def test_generate_diff(files):
-    pass
+import pytest
+
+from hexlet_code.parse_file import read_file
+
+file1, file2 = read_file
+
+
+def test_plain():
+    sample = './tests/fixtures/plain_result.txt'
+
+    with open(sample) as sample:
+        sample_content = sample.read()
+    assert generate_diff(file1, file2) != sample_content
