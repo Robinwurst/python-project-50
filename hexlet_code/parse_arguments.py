6d25da6b133027b0f4a9baac20049bba86ccc22a
robert <Neorganizm911@yandex.ru>
2024-09-22 16:24:19 +0300

    formats

--- hexlet_code/parse_arguments.py
+++ hexlet_code/parse_arguments.py
@@ -2,8 +2,11 @@ import argparse
 
 
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str, help='path to the first file')
-    parser.add_argument('second_file', type=str, help='path to the second file')
-    parser.add_argument('-f', '--format', type=str, default='stylish', help='set format of output')
+    parser.add_argument('second_file', type=str,
+                        help='path to the second file')
+    parser.add_argument('-f', '--format', type=str,
+                        default='stylish', help='set format of output')
     return parser.parse_args()
