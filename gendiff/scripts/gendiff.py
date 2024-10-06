2e9aff42aa6414f1ebcf9f572e26b55334a66e0e
robert <Neorganizm911@yandex.ru>
2024-10-06 23:23:00 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -13,7 +13,7 @@ from gendiff.generate_diff import generate_diff
 
 def main():
     args = parse_arguments()
-    diff = generate_diff(args.first_file, args.second_file)
+    diff = generate_diff(args.first_file, args.second_file, args.format)
     print(diff)
 
 
