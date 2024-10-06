0e814cffe9e475000ebb020e9c9e0d69efe1a500
robert <Neorganizm911@yandex.ru>
2024-10-06 21:47:10 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 from gendiff.parse_arguments import parse_arguments
-from gendiff.generate_diff import generate_diff
+# from gendiff.generate_diff import generate_diff
+from gendiff.difference import get_diff
+from gendiff.parse_file import read_file
+from gendiff.formats import formating
+
+
+def generate_diff(first_file_path, second_file_path, format_name='stylish'):
+    data1 = read_file(first_file_path)
+    data2 = read_file(second_file_path)
+    diff = get_diff(data1, data2)
+
+    return formating(diff, format_name)
 
 
 def main():
