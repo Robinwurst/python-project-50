0fa764dd90cc887ceb9e831d4a22c4a35aa1b604
robert <Neorganizm911@yandex.ru>
2024-10-06 22:20:10 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,7 +3,6 @@ from gendiff.parse_arguments import parse_arguments
 from gendiff.generate_diff import generate_diff
 
 
-
 # def generate_diff(first_file_path, second_file_path, format_name='stylish'):
 #     data1 = read_file(first_file_path)
 #     data2 = read_file(second_file_path)
