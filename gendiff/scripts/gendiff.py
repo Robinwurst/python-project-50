55e0776837b49af66ce735b0cbd49d2ddfe5f879
robert <Neorganizm911@yandex.ru>
2024-10-13 21:15:16 +0300

    fix

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
+import sys
 from gendiff.parse_arguments import parse_arguments
 from gendiff.generate_diff import generate_diff
 
 
 def main():
-    args = parse_arguments()
-    diff = generate_diff(args.first_file, args.second_file, args.format)
-    print(diff)
+    try:
+        args = parse_arguments()
+        diff = generate_diff(args.first_file, args.second_file, args.format)
+        print(diff)
+        sys.exit(0)
+    except ValueError as e:
+        print(e)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
